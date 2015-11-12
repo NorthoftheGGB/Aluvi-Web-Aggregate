b266193f7acc0e293c632c484a1c66dc77d3785b
Algernon <macster@algernon.pk5001z>
2015-11-12 12:00:36 -0800

    form destination 2

--- add_user.php
+++ add_user.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect("aluvi1.cr8zsnsf7jfy.us-west-2.rds.amazonaws.com", "master", "^cy*(b%ji%i", "aluvidb", 3306);
-if (!$conn) {
+$con = mysqli_connect("aluvi1.cr8zsnsf7jfy.us-west-2.rds.amazonaws.com", "master", "^cy*(b%ji%i", "aluvidb", 3306);
+if (!$con) {
 	echo "Could not establish connection to database";
 	exit;
 } 
@@ -8,8 +8,8 @@ $email = $_POST['email'];
 $name = $_POST['name'];
 $zip = $_POST['zip'];
 $driver = 0 + $_POST['driver'];
-mysqli_query("insert into users values('$name', '$email', '$zip', $driver");
-if ($e = mysqli_error($con)){
+if (!mysqli_query($con, "insert into users values('$name', '$email', '$zip', $driver")){
+$e = mysqli_error($con);
     echo "<!--$e--><h1>Sorry!</h1>";
 }
 else include "map.html";
