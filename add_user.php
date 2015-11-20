9d34b04735379d55e5240ad6bd7d8d1af412ede1
Algernon <macster@algernon.pk5001z>
2015-11-20 13:13:21 -0800

    transport modes 1

--- add_user.php
+++ add_user.php
@@ -12,9 +12,22 @@ if ($_POST['driver'])
 else
     $driver = 0;
 mysqli_query($con, $q = "insert into users values('$name', '$email', '$zip', $driver)");
-if ($e = mysqli_error($con)){
-    echo "<!--$e:
-    $q-->
-    <h1>Sorry!</h1>";
+mysqli_select_db($con, "glassdoor");
+$t_results = array();
+$bus_results = mysqli_query($con, $q1 = "select * from bus b join zip_codes z on st_intersects(b.geo, z.geo) where zip_code = $zip");
+if ($e = mysqli_error($con)) echo "<!-- $e FROM $q -->";
+while ($row = msqyli_fetch_array($bus_results, MYSQLI_ASSOC)){
+	$t_results['bus']['coordinates'][] = array($row['stop_lat'], $row['stop_lng']);
 }
-else include "map.html";
+/*
+$bart_ferry_results = mysqli_query($con, $q1 = "select * from bart_ferry f join zip_codes z on st_intersects(f.geo, z.geo) where zip_code = $zip");
+while ($row = msqyli_fetch_array($bart_ferry_results, MYSQLI_ASSOC)){
+	$t_results['ferry']['coordinates'][] = array($row['stop_lat'], $row['stop_lng']);
+}
+$bus_ferry_results = mysqli_query($con, $q1 = "select * from bus_ferry f join zip_codes z on st_intersects(f.geo, z.geo) where zip_code = $zip");
+while ($row = msqyli_fetch_array($bus_ferry_results, MYSQLI_ASSOC)){
+	
+}
+*/
+$transportationModes = json_encode($t_results); 
+include "map.php";
