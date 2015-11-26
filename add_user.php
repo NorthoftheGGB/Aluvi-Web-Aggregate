8ae912b96ea57cf378d8b04952529afb83cedeb9
Algernon <macster@Algernon.local>
2015-11-26 12:50:35 -0800

    bloody pain

--- add_user.php
+++ add_user.php
@@ -21,7 +21,7 @@ while ($row = mysqli_fetch_array($bus_results, MYSQLI_ASSOC)){
 	$t_results['bus']['coordinates'][] = array($row['stop_lat'], $row['stop_lon']);
 	$stop_info[] = "<a href='#'>$row[stop_name]</a>";
 }
-
+/*
 $bart_ferry_results = mysqli_query($con, $q1 = "select * from bart_ferry f join zip_codes z on st_intersects(f.geo, z.geo) where zip_code = $zip");
 if ($row = mysqli_fetch_array($bart_ferry_results, MYSQLI_ASSOC)){
 	$t_results['ferry']['coordinates'][] = array(37.795748, -122.393326);
@@ -33,7 +33,7 @@ if ($row = mysqli_fetch_array($bus_ferry_results, MYSQLI_ASSOC)){
 	$t_results['ferry']['coordinates'][] = array(37.856561, -122.478122);
 	$ferry_results = true;
 }
-
+*/
 $transportationModes = json_encode($t_results);
 $zip_results = mysqli_query($con, $qx = "select st_astext(geo) as geotext from zip_codes where zip_code = $zip");
 while ($row = mysqli_fetch_array($zip_results, MYSQLI_ASSOC)){
