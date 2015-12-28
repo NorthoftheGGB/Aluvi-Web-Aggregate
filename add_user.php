9c0af2e05bf4bb8052b04bb4a0bf0982ca9f7178
Your Name <you@example.com>
2015-12-28 23:15:00 +0000

    Moved database connect to a different file

--- add_user.php
+++ add_user.php
@@ -1,9 +1,5 @@
 <?php
-$con = mysqli_connect("aluvi1.cr8zsnsf7jfy.us-west-2.rds.amazonaws.com", "master", "^cy*(b%ji%i", "aluvidb", 3306);
-if (!$con) {
-	echo "Could not establish connection to database";
-	exit;
-}
+require('database.php');
 $email = $_REQUEST['email'];
 $name = $_REQUEST['name'];
 $zip = $_REQUEST['zip'];
---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect("aluvi1.cr8zsnsf7jfy.us-west-2.rds.amazonaws.com", "master", "^cy*(b%ji%i", "aluvidb", 3306);
+if (!$con) {
+	echo "Could not establish connection to database";
+	exit;
+}
