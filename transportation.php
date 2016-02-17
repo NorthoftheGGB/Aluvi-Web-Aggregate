e95de8b03c3e0895f70d56c986e10a907ce4d1e5
Algernon <macster@algernon.domain>
2016-02-17 09:38:52 -0800

    will try this one more time 2

--- transportation.php
+++ transportation.php
@@ -1,9 +1,8 @@
 <?php
-
-$context = $_GET['context'];
-
 require('vendor/autoload.php');
+$context = $_GET['context'];
 require('database.php');
+
 
 $link_key = $_GET['token'];
 $result = mysqli_query($users_con, $q = "select * from users where link_key = '$link_key'");
@@ -24,8 +23,9 @@ if(mysqli_num_rows($result) == 0){
 		$factory = new RandomLib\Factory;
 		$generator = $factory->getMediumStrengthGenerator();
 		$session_key = $generator->generateString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+		mysqli_query($users_con, $q = "update users set cookie_key='$session_key' where email = '${row['email']}'");
 		setcookie('aluvi_token', $session_key, time() + 30*60);
-		mysqli_query($users_con, $q = "update users set cookie_key='${_COOKIE['aluvi_token']}' where email = '${row['email']}'");
+
 		header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 		header("Pragma: no-cache"); // HTTP 1.0.
 		header("Expires: 0"); // Proxies.
