565cf037e17d5b6bbf5cf806caf3357857ff97aa
Algernon <macster@algernon.domain>
2016-02-17 09:35:20 -0800

    will try this one more time

--- update_preferences.php
+++ update_preferences.php
@@ -3,27 +3,13 @@ $context = $_POST['context'];
 require('vendor/autoload.php');
 require('database.php');
 //??
-/*
 $cookie_key = $_COOKIE['aluvi_token'];
 $result = mysqli_query($users_con, $q = "select * from users where cookie_key = '$cookie_key'");
 if(mysqli_num_rows($result) == 0){
-	echo "<!-$q-->";
-	require 'expired_link.php';
-	exit;
-}
-*/
-//$result = mysqli_query($users_con, $q = "select * from users where id = ${_POST['userid']}");
-
-if(mysqli_num_rows($result) == 0){
-	echo "<!-$q-->";
 	require 'expired_link.php';
 	exit;
 }
 
-if ($result['office'] == 'San Rafael')
-	$transitlink = "http://www.goldengatetransit.org/";
-else
-	$transitlink = "http://www.vta.org/";
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $userzip = $row['zip'];
 $userid = $row['id'];
@@ -36,9 +22,8 @@ $public_transportation = isset($_POST['transportation_type_public_transportation
 $commuter_bus =  isset($_POST['transportation_type_commuter_bus']) ? 1 : 0;
 mysqli_stmt_bind_param($stmt, 'iiiiiissss', $row['id'],  $carpool, $vanpool, $bicycle, $public_transportation, $commuter_bus, $_POST['carpool_options'], $_POST['vanpool_options'], $t1 = $_POST['carpool_times_morning'], $t2 = $_POST['carpool_times_evening']);
 mysqli_stmt_execute($stmt);
-
-if ($context )
 // and get the map data ready
+$t_results = array('bus' => array('coordinates' => array()));
 if ($carpool){
 	$carpool_matches = array();
 	$n_results = 0;
@@ -51,7 +36,27 @@ if ($carpool){
 			$carpool_matches[] = "<tr><td>$row[name]</td><td>$row[email]</td><td>$row[carpool_option]</td><td>$row[t1]am</td><td>$row[t2]pm</td></tr>";
 	}
 }
+if ($vanpool){
+	$vanpool_matches = array();
+	$van_results = mysqli_query($con, $qx = "select u.name, email, location_title title, departs_location dl, arrives_work aw, departs_work dw, lat, lng
+				    from vanpool_pickup p left join aluvidb.users u on leader_id = u.id join zip_codes z on z.zip_code = p.zip
+				    join (select geo from zip_codes where zip_code = $userzip) sq
+				    where p.zip = $userzip or st_touches(z.geo, (sq.geo)) order by p.zip = $userzip desc limit 3");
+	//echo "<!--$qx-->";
+	if ($dl = $row['dl']) $dl .= 'am'; else $dl = 'varies';
+	if ($aw = $row['aw']) $aw .= 'am'; else $aw = 'varies';
+	if ($dw = $row['dw']) $dw .= 'pm'; else $dw = 'varies';
+	if (!$name = $row['name']) $name = "Chariot";
+	if (!$email = $row['email']) $email = "www.chariot.com";
+	while ($row = mysqli_fetch_array($van_results)){
+		$vanpool_matches[] =  "<tr><td>$name</td><td>$email</td><td>$row[title]</td><td>$dl</td><td>$aw</td><td>$dw</td></tr>";
+		$t_results['vanpool']['coordinates'][] = array($row['lat'], $row['lng']);
+	}
+}
 
+
+
+$transportationModes = json_encode($t_results);
 $zip_results = mysqli_query($con, $qx = "select st_astext(geo) as geotext from zip_codes where zip_code = $zip");
 while ($row = mysqli_fetch_array($zip_results, MYSQLI_ASSOC)){
 	$raw_coordinates = str_replace('MULTIPOLYGON', '', str_replace('(', '', str_replace(')', '', $row['geotext'])));
