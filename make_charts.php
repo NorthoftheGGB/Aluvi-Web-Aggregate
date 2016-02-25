92bd94d6a9f24fe77431de62bccd37201bbad299
Algernon <macster@Algernon.local>
2016-02-25 14:18:15 -0800

    test charts 4

--- make_charts.php
+++ make_charts.php
@@ -1,13 +1,14 @@
 <?php
+include "database.php";
 function addChart($element, $title, $data, $data_header, $type){
-foreach ($data as $d){
-	$data_fmt[] = "['$d[0]', $d[1]]";
+foreach ($data as $k => $v){
+	$data_fmt[] = "['$k', $v]";
 }
 $data_fmt = implode(",", $data_fmt);
 echo
 "
         data = google.visualization.arrayToDataTable([
-          $data_header
+          $data_header,
           $data_fmt
         ]);
         options = {
@@ -17,22 +18,23 @@ echo
         chart.draw(data, options);
 ";
 }
+$data1 = myesqli_fetch_assoc(mysqli_query("select sum(carpool) as Carpool, sum(vanpool) as Vanpool, sum(public_transportation) as `Public Transportation` from preferences"));
 ?>
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
       google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-	<?php addChart('pie1', 'Mentalism', array(array('pox', 23),array('lim', 50),array('arum', 17)), "['ilp', 'yilx'],", 'PieChart') ?>
-	<?php addChart('pie2', 'Totalism', array(array('pax', 13),array('lim', 30),array('arum', 27)), "['ilp', 'yilx'],", 'ColumnChart') ?>
+      google.charts.setOnLoadCallback(drawCharts);
+      function drawCharts() {
+	<?php addChart('column1', 'Globally', $data1, "['ilp', 'yilx']", 'ColumnChart') ?>
+	<?php addChart('pie2', 'Totalism', array('pax' => 13, 'zim' => 25, 'yilf' => 18), "['ilp', 'yilx']", 'PieChart') ?>
         
       }
     </script>
   </head>
   <body>
-    <div id="pie1" style="width: 450px; height: 250px;"></div>
+    <div id="column1" style="width: 450px; height: 250px;"></div>
     <div id="pie2" style="width: 450px; height: 250px;"></div>
   </body>
 </html>
