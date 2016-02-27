84c8c77d3149598ec1f743a9a31151d313dd6fca
Algernon <macster@Algernon.local>
2016-02-27 15:01:37 -0800

    demo charts csv 2

--- demo_csv.php
+++ demo_csv.php
@@ -3,10 +3,10 @@ $context = "demo";
 include "database.php";
 $query = "select date_format(added, '%m/%d/%Y')as added, name, email, carpool_times_morning, carpool_times_evening, case when carpool then 'yes' else 'no' end, case when vanpool then '
 yes' else 'no' end, case when public_transportation then 'yes' else 'no' end from users u join preferences p on u.id = user_id";
-$csv = "Date Added,Name,Email,Arrives,Departs,Carpool,Vanpool,Public Transportation";
-$result = mysqli_query($query);
+$csv = "Date Added,Name,Email,Arrives,Departs,Carpool,Vanpool,Public Transportation\n";
+$result = mysqli_query($users_con, $query);
 while ($row = mysqli_fetch_assoc($result)){
-    $csv .= implode(",", $row);
+    $csv .= implode(",", $row).'\n';
 }
 header("Content-Disposition: attachment; filename='users.csv'"); 
 header("Content-Type: application/csv");
