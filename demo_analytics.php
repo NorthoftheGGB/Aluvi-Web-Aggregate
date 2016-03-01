d00d3e48658ded92c59626c48fe64be08ddbeba2
Algernon <macster@Algernon.local>
2016-03-01 09:32:26 -0800

    broken out 2

--- demo_analytics.php
+++ demo_analytics.php
@@ -1,8 +1,8 @@
 <html>
 	<head></head>
 	<body>
-		<iframe style="width:100%; height:500px" frameborder='0' src='demo_charts.php'></iframe>
 		<iframe style="width:100%; height:400px" frameborder='0' src='demo_charts.php'></iframe>
+		<iframe style="width:100%; height:400px" frameborder='0' src='demo_heatmap.php'></iframe>
 	</body>
 </html>
    
