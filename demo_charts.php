f7c5d1acf5eb1dae7cd20171c0fad51414b54413
Algernon <macster@Algernon.local>
2016-02-29 19:50:42 -0800

    putas del diablo

--- demo_charts.php
+++ demo_charts.php
@@ -83,41 +83,7 @@ $heatmap_data = implode(",", $heatmap_data);
 	$('.city_'+city).show();
       }
     </script>
-        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=visualization"></script>
-    <!--
-    <script>
-var map, pointarray, heatmap;
- 
-var data = [
-  <?php echo $heatmap_data ?>
-];
- 
- 
-function initialize() {
-  // the map's options
-  var mapOptions = {
-    zoom: 11,
-    center: new google.maps.LatLng(37.784546, -122.433523)  };
- 
-  // the map and where to place it
-  map = new google.maps.Map(document.getElementById('heatmap'), mapOptions);
- 
-  var pointArray = new google.maps.MVCArray(data);
- 
-  // what data for the heatmap and how to display it
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: pointArray,
-    radius: 50
-  });
- 
-  // placing the heatmap on the map
-  heatmap.setMap(map);
-}
- 
-// as soon as the document is ready the map is initialized
-google.maps.event.addDomListener(window, 'load', initialize);
- 
-    </script>-->
+  
     <style>
 	.chart {float:left;}
 	.cityopt, .zipopt {display:none;}
