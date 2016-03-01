aef0aaef49c1a0f41c214b9c72a86bae653dd44b
Algernon <macster@Algernon.local>
2016-02-29 18:19:24 -0800

    select locations 1

--- demo_charts.php
+++ demo_charts.php
@@ -2,6 +2,18 @@
 $context = "demo";
 include "database.php";
 include "make_charts.php";
+//geographical data
+$result = mysqli_query($con, "select u.zip, c.name city_name, c.id as city_id, cn.name county_name, cn.id as county_id
+		       from demo_users.users u join demo_users.preferences p on p.user_id = u.id
+		       join zipcode_locations zl on zl.zip = u.zip join cities c on c.id = zl.city join counties cn on cn.id = c.county group by u.zip
+		       order by cn.name, c.name, u.zip");
+while ($row = mysqli_fetch_array($result)){
+	$zipcodes[] = array("code" => $row['zip'], "city" => $row['city_id']);
+	$cities[$row['city_id']] = array("name" => $row['city_name'], "id" => $row['city_id'], "county" => $row['county_id']);
+	$counties[$row['county_id']] = array("name" => $row['county_name'], "id" => $row['county_id'] );
+}
+
+//user data
 $q1 = "select sum(carpool) as Carpool, sum(vanpool) as Vanpool, sum(public_transportation) as `Public Transportation` from preferences";
 $tq1 = "select distinct(carpool_times_morning) as time from preferences order by time";
 $tq2 = "select distinct(carpool_times_evening) as time from preferences order by time";
@@ -52,6 +64,29 @@ $users = mysqli_fetch_assoc(mysqli_query($users_con, $uq));
 	</div>
 	<br/><br/><br/>
 	<div style='width:1060px; margin:auto'>
+	<form method = 'get' action = 'demo_charts.php'>
+		<select name='county' onchange='showCities(this.value)'>
+			<option value='all'>All Counties</option>
+			<?php foreach ($counties as $c){
+				echo "<option value='$c[id]'>$c[name]</option>";
+				}
+			?>
+		</select>
+		<select name='city' onchange='showZipcodes(this.value)'>
+			<option>All Cities</option>
+			<?php foreach ($cities as $c){
+				echo "<option value='$c[id]' class='cityopt county_$c[county]'>$c[name]</option>";
+				}
+			?>
+		</select>
+		<select name='zipcode' onchange='showCounties(this.value)'>
+			<option value='all'>All Zip Codes</option>
+			<?php foreach ($zipcodes as $z){
+				echo "<option value='$z[id]' class='zipopt city_$z[city]'>$z[name]</option>";
+				}
+			?>
+		</select>
+	</form>
 	<div class="chart" id="column1" style="width: 420px; height: 250px;"></div>
 	<div  class="chart" id="pie1" style="width: 320px; height: 250px;"></div>
 	<div  class="chart" id="pie2" style="width: 320px; height: 250px;"></div>
