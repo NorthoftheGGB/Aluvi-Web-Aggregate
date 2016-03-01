3a7abb0f8b8ec14cb005855a4a61e8bfbec9f319
Algernon <macster@Algernon.local>
2016-03-01 09:30:00 -0800

    broken out

--- demo_charts.php
+++ demo_charts.php
@@ -50,13 +50,7 @@ $data1 = mysqli_fetch_assoc(mysqli_query($users_con, $q1));
 $data2 = mysqli_fetch_assoc(mysqli_query($users_con, $q2));
 $data3 = mysqli_fetch_assoc(mysqli_query($users_con, $q3));
 $users = mysqli_fetch_assoc(mysqli_query($users_con, $uq));
-//heatmap
-$result = mysqli_query($users_con, "select lat, lng from glassdoor.zipcode_locations z join users u on u.zip = z.zip
-					  join preferences p on u.id = p.user_id");
-while ($row = mysqli_fetch_assoc($result)) {
-	$heatmap_data[] = "new google.maps.LatLng($row[lat], $row[lng])";
-}
-$heatmap_data = implode(",", $heatmap_data);
+
 ?>
 <html>
   <head>
@@ -82,42 +76,7 @@ $heatmap_data = implode(",", $heatmap_data);
 	$('#zipsel').val('all');
 	$('.city_'+city).show();
       }
-    </script>
-        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=visualization"></script>
-    
-    <script>
-var map, pointarray, heatmap;
- 
-var data = [
-  <?php echo $heatmap_data ?>
-];
- 
- 
-function initialize() {
-  // the map's options
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(37.794546, -122.433523)  };
- 
-  // the map and where to place it
-  map = new google.maps.Map(document.getElementById('heatmap'), mapOptions);
- 
-  var pointArray = new google.maps.MVCArray(data);
- 
-  // what data for the heatmap and how to display it
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: pointArray,
-    radius: 50
-  });
- 
-  // placing the heatmap on the map
-  heatmap.setMap(map);
-}
- 
-// as soon as the document is ready the map is initialized
-google.maps.event.addDomListener(window, 'load', initialize);
- 
-    </script>
+    </script>        
     <style>
 	.chart {float:left;}
 	.cityopt, .zipopt {display:none;}
@@ -163,9 +122,6 @@ google.maps.event.addDomListener(window, 'load', initialize);
 	<div  class="chart" id="pie1" style="width: 320px; height: 250px;"></div>
 	<div  class="chart" id="pie2" style="width: 320px; height: 250px;"></div>
 	</div>
-	<center>
-		<div style = "width:800px; height:400px;" id="heatmap"></div>
-	</center>
   </body>
 </html>
    
---
+++ demo_heatmap.php
@@ -0,0 +1,56 @@
+<?php
+$context = "demo";
+include "database.php";
+//heatmap
+$result = mysqli_query($users_con, "select lat, lng from glassdoor.zipcode_locations z join users u on u.zip = z.zip
+					  join preferences p on u.id = p.user_id");
+while ($row = mysqli_fetch_assoc($result)) {
+	$heatmap_data[] = "new google.maps.LatLng($row[lat], $row[lng])";
+}
+$heatmap_data = implode(",", $heatmap_data);
+?>
+<html>
+  <head>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=visualization"></script>
+    <script>
+var map, pointarray, heatmap;
+ 
+var data = [
+  <?php echo $heatmap_data ?>
+];
+ 
+ 
+function initialize() {
+  // the map's options
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(37.794546, -122.433523)  };
+ 
+  // the map and where to place it
+  map = new google.maps.Map(document.getElementById('heatmap'), mapOptions);
+ 
+  var pointArray = new google.maps.MVCArray(data);
+ 
+  // what data for the heatmap and how to display it
+  heatmap = new google.maps.visualization.HeatmapLayer({
+    data: pointArray,
+    radius: 50
+  });
+ 
+  // placing the heatmap on the map
+  heatmap.setMap(map);
+}
+ 
+// as soon as the document is ready the map is initialized
+google.maps.event.addDomListener(window, 'load', initialize);
+ 
+    </script>
+  </head>
+  <body>
+	
+	<center>
+		<div style = "width:800px; height:400px;" id="heatmap"></div>
+	</center>
+  </body>
+</html>
+   
