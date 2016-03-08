8b224906dea60debf7233065293e35eca47b540e
Algernon <macster@Algernon.local>
2016-03-07 17:33:33 -0800

    new admin 1.32

--- analytics/demo_admin.php
+++ analytics/demo_admin.php
@@ -1,4 +1,5 @@
 <?php
+echo "<!-- $_POST[action]-->";
 if ($_POST['action'] == 'add user'){
     $name = mysqli_real_escape_string($users_con, $_POST['name']);
     mysqli_query($users_con, $Q = "insert into admin (name, email) values ('$name', '$_POST[email])'");
