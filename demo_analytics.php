f34ad9f2d95f8cbfd2ab9ca8e778320f9410b252
Algernon <macster@Algernon.local>
2016-03-09 15:34:37 -0800

    offices 1.21

--- demo_analytics.php
+++ demo_analytics.php
@@ -11,7 +11,7 @@ foreach ($office_results as $r){
 	$selected = $office == $r['id'] ? 'selected' : '';
 	if ($_GET['view'])
 		$url .= '&view='.$_GET['view'];
-	$office_options .= "<option onclick='window.location=\"$url\"'>$r[name]</option>";
+	$office_options .= "<option onclick='window.location=\'$url\''>$r[name]</option>";
 }
 ?>
 <html>
