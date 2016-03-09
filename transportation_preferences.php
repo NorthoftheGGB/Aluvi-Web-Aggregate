fbc7025e52586d4822d6044a616d281c0d8b37a6
Algernon <macster@Algernon.local>
2016-03-09 11:03:57 -0800

    transit preerences 1.2

--- transportation_preferences.php
+++ transportation_preferences.php
@@ -153,7 +153,7 @@ echo "<input type='hidden' name='context' value='$context'>
 </div>
 
 <?php } else echo "<div class='option' style='width:300px'>&nbsp;</div>" ?>
-<center style='width:1000px;' class="times_select">
+<center style='width:1000px; margin-bottom:80px' class="times_select">
 <div style='width:800px'>
 <p style='float:left; margin-left:130px; margin-right:10px;'>
 When I'd like to arrive at work (AM):&nbsp;&nbsp;<br>
