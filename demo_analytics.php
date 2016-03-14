573321d1678475cca3da687465944bad6b3c1927
Algernon <macster@algernon.t-mobile.com>
2016-03-13 17:52:23 -0700

    design experiments 2.4

--- demo_analytics.php
+++ demo_analytics.php
@@ -25,29 +25,31 @@ foreach ($office_results as $r){
 			<br/><img src='AluviWhite.png' /><br/><br/>
 			
 		</center>
-		<center>
-			<select onchange='window.location = $(this).val()'>
-				<?php echo $office_options ?>
-			</select>
-		</center>
-		<br />
-		<div class='tabs'>
-			
-			<?php foreach(array("Survey", "Recommendations",  "Usage", "Users", "Sustainability", "Options", "Admin") as $tab){
-				if ($tab == $_GET['view']) $selected = "class='selected'";
-					else $selected = "";
-				echo "<a href='$main_url&view=$tab' $selected>$tab</a>";
-				}?>
+		<div style="margin:10px">
+			<center>
+				<select onchange='window.location = $(this).val()'>
+					<?php echo $office_options ?>
+				</select>
+			</center>
+			<br />
+			<div class='tabs'>
+				
+				<?php foreach(array("Survey", "Recommendations",  "Usage", "Users", "Sustainability", "Options", "Admin") as $tab){
+					if ($tab == $_GET['view']) $selected = "class='selected'";
+						else $selected = "";
+					echo "<a href='$main_url&view=$tab' $selected>$tab</a>";
+					}?>
+			</div>
+			<?php if (!$_GET['view'] || $_GET['view'] == 'Survey')  { ?>
+			<iframe style="width:100%; height:340px" frameborder='0' src='analytics/demo_charts.php?office=<?php echo $office ?>'></iframe>
+			<iframe style="width:100%; height:450px" frameborder='0' src='analytics/demo_heatmap.php?office=<?php echo $office ?>'></iframe>
+			<?php } else if (in_array($_GET['view'], array('Users', "Recommendations", "Admin", "Options"))) {
+				$view = strtolower($_GET['view']);
+				include "analytics/demo_$view.php";
+			}
+			else echo "<center><b>Coming Soon</b></center>";
+			?>
 		</div>
-		<?php if (!$_GET['view'] || $_GET['view'] == 'Survey')  { ?>
-		<iframe style="width:100%; height:340px" frameborder='0' src='analytics/demo_charts.php?office=<?php echo $office ?>'></iframe>
-		<iframe style="width:100%; height:450px" frameborder='0' src='analytics/demo_heatmap.php?office=<?php echo $office ?>'></iframe>
-		<?php } else if (in_array($_GET['view'], array('Users', "Recommendations", "Admin", "Options"))) {
-			$view = strtolower($_GET['view']);
-			include "analytics/demo_$view.php";
-		}
-		else echo "<center><b>Coming Soon</b></center>";
-		?>
 	</body>
 </html>
    
