e3d0355e279c7e49c4ccb9a0bd59f71c8d8a6d4a
Algernon <macster@Algernon.local>
2016-03-28 15:58:29 -0700

    new admin 2

--- admin/admin.php
+++ admin/admin.php
@@ -1,31 +1,34 @@
 <?php
 if ($_POST['action']){
     $name = mysqli_real_escape_string($users_con, $_POST['name']);
-    mysqli_query($users_con, $Q = "insert into admin (name, email) values ('$name', '$_POST[email]')");
+    $salt = substr(hash('sha512',uniqid(rand(), true).$key.microtime()), 15);
+    $password = hash('sha512', $salt.$_POST['password']);
+    mysqli_query($users_con, $Q = "insert into admin.users (name, password, salt, context) values ('$name', '$password', '$salt', '$context')");
 }
 
-$admin_results = mysqli_query($users_con, "select name, email from admin");
+$admin_results = mysqli_query($users_con, "select name, context from admin");
 ?>
 <form method='post' action='<?php echo $main_url?>&view=Admin'>
 <input type='hidden' name='action' value='add user' />
 <h2>Admins</h2>
 <table style='width:30%'>
     <tr>
-        <th>Name</th>
-        <th>Email</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Company</th>
     </tr>
     <?php
         while ($row = mysqli_fetch_assoc($admin_results)){
-        echo "<tr>";
-            foreach ($row as $c){
-                echo "<td>$c</td>";
-            }
-        }
+        echo "<tr>";       
+        echo "<td>$row[name]</td>";
+        echo "<td>$row[context]</td>";
+        echo "<td>*******</td>";
         echo "</tr>";
     ?>
     <tr class='input'>
-        <td><input style='width:100%' type='text' placeholder='Name' name='name'/></td>
-        <td><input style='width:100%' type='text' placeholder='Email' name='email'/></td>
+        <td><input style='width:100%' type='text' placeholder='Username' name='name'/></td>
+        <td><input style='width:100%' type='text' placeholder='Password' name='password'/></td>
+        <td><input style='width:100%' type='text' placeholder='Password' name='company'/></td>
     </tr>
 </table>
 <br/>
