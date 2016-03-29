b42908806980da1423b1fbee0fc83333ee0a3574
Algernon <macster@Algernon.local>
2016-03-28 17:16:52 -0700

    new admin 3.1

---
+++ admin/auth.php
@@ -0,0 +1,34 @@
+<?php
+if (!$_SESSION)
+    session_start();
+if ($_POST['username']) {
+    $admin_con = mysqli_connect("aluvi1.cr8zsnsf7jfy.us-west-2.rds.amazonaws.com", "master", "^cy*(b%ji%i", "aluvidb", 3306);
+    if (!$admin_con) {
+	echo "Could not establish connection to database";
+	exit;
+    }
+    mysqli_select_db($admin_con, "admin");
+    $result = mysqli_query($admin_con, "select * from users where name='$_POST[username]'");
+    while ($row = mysqli_fetch_assoc($result)){
+        $password = hash('sha512', $row['salt'].$_POST['password']);
+        if ($password == $row['password']){
+            $_SESSION['context'] = $row['context'];
+            break;
+        }
+        $error = "Name and/or password incorrect.";
+    }
+    mysqli_close($admin_con);
+}
+if ($_SESSION['context']){
+    if ($_SESSION['context'] == 'super'){
+        $super = true;
+        $domsplat = explode('.', $_SERVER['HTTP_HOST']);
+        $context = $domsplat[0];
+    }
+    else
+        $context = $_SESSION['context'];
+}
+if (!$context){
+    include "login.php";
+    exit;
+}
---
+++ admin/login.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<link href="admin.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<center id="logo">
+			<br/><img src='../AluviGradient.png' /><br/><br/>	
+		</center>
+                <form method='post' action='index.php'>
+                    <p style='color:red'><?php echo $error ?>&nbsp;</p>
+                    <input type='hidden' name='office' value='<?php echo $_GET["office"] ?>' />
+                    <input type='hidden' name='view' value='<?php echo $_GET["view"] ?>' />
+                    <input type='text' name='username' placeholder='Username' /><br/>
+                    <input type='text' name='password' value='password' /><br/>
+                    <input type='submit' value='Log in' />
+                </form>
+        </body>
+</html>
