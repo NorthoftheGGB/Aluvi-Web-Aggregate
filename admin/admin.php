828451540bdd6c28155230ba6b74b1785dcc3eb2
Algernon <macster@Algernon.local>
2016-03-29 16:37:37 -0700

    global backend 3

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,10 @@
 <?php
 if (!$super)
 echo "<center><b>Access Denied</b></center>";
+exit;
+if ($delid = $_GET['delete']){
+    mysqli_query($users_con, "delete from admin where id = $delid");
+}
 if ($_POST['new_password']){
     $name = mysqli_real_escape_string($users_con, $_POST['name']);
     $salt = substr(hash('sha512',uniqid(rand(), true).microtime()), 0, 15);
@@ -10,6 +14,13 @@ if ($_POST['new_password']){
 
 $admin_results = mysqli_query($users_con, "select name, id from admin.users where context = '$context'");
 ?>
+<script type='text/javascript'>
+    function deletePrompt (id, name) {
+        if (confirm("Delete user "+name+"?")){
+            window.location="<?php echo $main_url?>&view=Admin&delete="+id;
+        }
+    }
+</script>
 <form method='post' action='<?php echo $main_url?>&view=Admin'>
 <input type='hidden' name='action' value='add user' />
 <h2>Admins</h2>
@@ -23,7 +34,7 @@ $admin_results = mysqli_query($users_con, "select name, id from admin.users wher
         echo "<tr>";       
         echo "<td>$row[name]</td>";
         echo "<td>*******</td>";
-        echo "<td></td>";
+        echo "<td><a href='javascript:deletePrompt($row[id], $row[name])'>Delete</a></td>";
         echo "</tr>";
         }
     ?>
