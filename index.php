019e49bcdef0ba977708b82d84ec77496008ca5c
Algernon <macster@Algernon.local>
2016-03-29 16:51:14 -0700

    all to one

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
 $domsplat = explode('.', $_SERVER['HTTP_HOST']);
 $context = $domsplat[0];
-if ($context == 'saas'){
-    $context = $_GET['context'];
-    if (!$context)
-        include "demo.html";
-    else
-        include "index_content.php";
-}
-else if ($context == 'fico'){
-    include "fico.html";
-}
-else include "index_content.php";
+include "index_content.php";
