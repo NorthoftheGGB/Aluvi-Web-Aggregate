be370db112ba9a4d849c7e0e688a5834b93be3ae
Algernon <macster@Algernon.local>
2016-03-29 13:31:13 -0700

    backend changes 4.51

--- admin/login.php
+++ admin/login.php
@@ -6,13 +6,13 @@
 		<center id="logo">
 			<br/><img src='../AluviGradient.png' /><br/><br/>	
 		</center>
-                <div style='width:300px; margin:auto;'>
+                <div style='width:130px; margin:auto;'>
                 <form method='post' action='index.php'>
                     <p style='color:red'><?php echo $error ?>&nbsp;</p>
                     <input type='hidden' name='office' value='<?php echo $_GET["office"] ?>' />
                     <input type='hidden' name='view' value='<?php echo $_GET["view"] ?>' />
-                    <input type='text' name='username' placeholder='Username' /><br/><br/>
-                    <input type='password' name='password' placeholder='password' /><br/><br/>
+                    <input type='text' name='username' placeholder='Username' /><br/>
+                    <input type='password' name='password' placeholder='password' /><br/>
                     <input type='submit' value='Log in' />
                 </form>
                 </div>
