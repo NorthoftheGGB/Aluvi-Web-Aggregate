e88c022b6ca65d7b9a57f58a252f4bf0c999a142
Algernon <macster@Algernon.local>
2016-04-15 17:01:06 -0700

    verifone distinctions 3

--- update_preferences.php
+++ update_preferences.php
@@ -16,16 +16,15 @@ $userzip = $row['zip'];
 $userid = $row['id'];
 if ($context != 'demo')
 $office_coordinates = $office_coordinates[$row['office'] -1];
-
 mysqli_query($users_con, 'delete from preferences where user_id='.$row['id']);
-$stmt = mysqli_prepare($users_con, 'insert into preferences (user_id, carpool, vanpool, bicycle, walking, public_transportation, commuter_bus, carpool_option, vanpool_option, carpool_times_morning, carpool_times_evening) values ( ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?)');
+$stmt = mysqli_prepare($users_con, 'insert into preferences (user_id, carpool, vanpool, bicycle, walking, public_transportation, commuter_bus, carpool_option, vanpool_option, public_option, carpool_times_morning, carpool_times_evening) values ( ?, ?, ?, ?, ?, ?, ?, ? , ?, ?, ?, ?)');
 $carpool = isset($_POST['transportation_type_carpool']) ? 1 : 0;
 $vanpool = isset($_POST['transportation_type_vanpool']) ? 1 : 0; 
 $bicycle = isset($_POST['transportation_type_bicycle']) ? 1 : 0;
 $walking = isset($_POST['transportation_type_walking']) ? 1 : 0; 
 $public_transportation = isset($_POST['transportation_type_public_transportation']) ? 1 : 0;
 $commuter_bus =  isset($_POST['transportation_type_commuter_bus']) ? 1 : 0;
-mysqli_stmt_bind_param($stmt, 'iiiiiiissss', $row['id'],  $carpool, $vanpool, $bicycle, $walking, $public_transportation, $commuter_bus, $_POST['carpool_options'], $_POST['vanpool_options'], $t1 = $_POST['carpool_times_morning'], $t2 = $_POST['carpool_times_evening']);
+mysqli_stmt_bind_param($stmt, 'iiiiiiissssss', $row['id'],  $carpool, $vanpool, $bicycle, $walking, $public_transportation, $commuter_bus, $_POST['carpool_options'], $_POST['vanpool_options'], $_POST['public_options'], $t1 = $_POST['carpool_times_morning'], $t2 = $_POST['carpool_times_evening']);
 mysqli_stmt_execute($stmt);
 if ($context == 'demo'){
 	include "map_demo.html";
