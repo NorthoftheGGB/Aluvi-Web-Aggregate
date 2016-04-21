29150195415459d8762632c8666c7e895dee9596
Algernon <macster@Algernon.local>
2016-04-21 11:45:53 -0700

    comments 13

--- update_preferences.php
+++ update_preferences.php
@@ -29,7 +29,8 @@ mysqli_stmt_bind_param($stmt, 'iiiiiiisssss', $row['id'],  $carpool, $vanpool, $
 mysqli_stmt_execute($stmt);
 if ($comments_box){
 	$comments = mysqli_real_escape_string($users_con, $_POST['comments']);
-	mysqli_query($users_con, "update preferences set comments = '$comments' where userid = $row[id]");
+	mysqli_query($users_con, $q = "update preferences set comments = '$comments' where userid = $row[id]");
+echo $q;
 }
 if ($context == 'demo'){
 	include "map_demo.html";
